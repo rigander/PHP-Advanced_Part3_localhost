5464eec3fa2961ffc196556feb4d4b94e3c87f09
Andrey Komov <andrewkomov@gmail.com>
2023-03-09 19:20:19 +0200

    localhost -laba 3.2

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+header ("Content-Type: text/html; charset=utf-8");
+echo "<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+<title>Open Server</title>
+</head>
+<body style=\"background: url(fon.png) top left repeat-x\">
+<center>
+<br><br><br><div style=\"width: 600px;\"><span style=\"font-size: 32px; color: green; font-family: Arial, Verdana; text-shadow: 0 1px 0 #fff\">Добро пожаловать в Open Server!</span>
+<br><br><br><span style=\"font-size: 32px; color: #333; font-family: Verdana, Arial;\">Он работает ;-)</span>
+<br><img src=\"st.png\" style=\"margin: 40px 0\"><br><a href=\"http://open-server.ru/docs/\" style=\"font-size: 24px; color: #048acd; font-family: Arial;\">Руководство пользователя</a></span><br><br><br></div>
+</center>
+</body>
+</html>";
+?>
---
+++ rss_reader.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>News feed</title>
+	<meta charset="utf-8" />
+</head>
+<body style="background-color: #4d4d4e; color: #bd6f31;">
+
+<h1>Latest News</h1>
+<?php
+
+?>
+</body>
+</html>
